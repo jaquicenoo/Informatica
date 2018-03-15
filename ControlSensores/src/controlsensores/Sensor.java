172380f6afaaac69afca40e951d9bb8b59d27549
jaquicenoo <36316958+jaquicenoo@users.noreply.github.com>
2018-03-15 04:09:54 +0100

    comit stand alone
    se suben casos de uso stand alone y piloto aplicacion de control de sensores

---
+++ ControlSensores/src/controlsensores/Sensor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlsensores;
+
+/**
+ *
+ * @author u1401
+ */
+public class Sensor {
+    
+}
